8c64d7d40228a96467f0efc6ebf75ae3f1159bcb
Steven Indzeoski <steven.indzeoski@gmail.com>
2019-12-07 16:10:32 -0600

    Finished day 6 part 1.

--- day6/1.ts
+++ day6/1.ts
@@ -4,30 +4,44 @@ function parseInput(path: string): string[] {
   return readFileSync(path, {encoding: 'utf-8'}).split('\n');
 }
 
-function makeOrbitsMap(orbits: string[]): Map<string, Set<string>> {
-  const orbitsMap = new Map<string, Set<string>>();
+function makeOrbitsMap(orbits: string[]): [Map<string, string>, Set<string>] {
+  const orbitsMap = new Map<string, string>();
+  const allObjects = new Set<string>();
 
   orbits.forEach((orbit) => {
     if (orbit) {
       const [left, right] = orbit.split(')');
-      let orbitSet = orbitsMap.get(left);
-
-      if (!orbitSet) {
-        orbitSet = new Set<string>();
-        orbitsMap.set(left, orbitSet);
-      }
-
-      orbitSet.add(right);
+      allObjects.add(left);
+      allObjects.add(right);
+      orbitsMap.set(right, left);
     }
   });
 
-  return orbitsMap;
+  return [orbitsMap, allObjects];
+}
+
+function getOrbitsCount(object: string, orbitsMap: Map<string, string>): number {
+  const orbitting = orbitsMap.get(object);
+
+  if (!orbitting) {
+    return 0;
+  }
+
+  return 1 + getOrbitsCount(orbitting, orbitsMap);
 }
 
 function main() {
-  const orbits = parseInput('day6/testinput.txt');
-  const orbitsMap = makeOrbitsMap(orbits);
-  console.log(orbitsMap);
+  const orbits = parseInput('day6/input.txt');
+  const [orbitsMap, allObjects] = makeOrbitsMap(orbits);
+  // console.log(orbitsMap);
+  // console.log(allObjects);
+  let total = 0;
+
+  for (const object of allObjects) {
+    total += getOrbitsCount(object, orbitsMap);
+  }
+
+  console.log(total);
 }
 
 main();
